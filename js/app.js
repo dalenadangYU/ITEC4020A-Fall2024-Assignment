61bd79c1e0e6fd52684ecfabb7e6a1ae06d4c8c5
dalenadangYU <itsddang@my.yorku.ca>
2024-12-04 10:04:43 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function messageNA() {
+    alert("Sorry, this source is not available yet.  To be uploaded soon.");
+}
+
+function hover_start(img_id) {
+    document.getElementById(img_id).style.opacity = "1";
+}
+  
+function hover_stop(img_id) {
+    document.getElementById(img_id).style.opacity = "0";
+}

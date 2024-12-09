6a1cbd350e9b3bc2fd57ba34bececa2f75db4f4e
dalenadangYU <itsddang@my.yorku.ca>
2024-12-08 21:37:22 -0500

    Delete gptresearch directory
    Use chatgptefficiency folder

--- gptresearch/ITEC4020_dataset/importcollections.bat
+++
@@ -1,7 +0,0 @@
-mongoimport --db ChatGPT_Evaluation --collection Computer_Security --type csv --file computer_security_test.csv --fields question,optionA,optionB,optionC,optionD,anticipatedAnswer,chatGPTResponse --uri "mongodb+srv://itsddang:t1F0QY0kSetB0AQi@itec4020a.n1uno.mongodb.net/"
-
-mongoimport --db ChatGPT_Evaluation --collection History --type csv --file prehistory_test.csv --fields question,optionA,optionB,optionC,optionD,anticipatedAnswer,chatGPTResponse --uri "mongodb+srv://itsddang:t1F0QY0kSetB0AQi@itec4020a.n1uno.mongodb.net/"
-
-mongoimport --db ChatGPT_Evaluation --collection Social_Science --type csv --file sociology_test.csv --fields question,optionA,optionB,optionC,optionD,anticipatedAnswer,chatGPTResponse --uri "mongodb+srv://itsddang:t1F0QY0kSetB0AQi@itec4020a.n1uno.mongodb.net/"
-
-pause
